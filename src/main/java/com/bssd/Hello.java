27631fe4599965e76e63118737437fb05bb22173
Kevin Clevenger <kclevenger@bssd.net>
2021-05-26 11:36:20 -0500

    added files

---
+++ src/main/java/com/bssd/Hello.java
@@ -0,0 +1,10 @@
+package com.bssd;
+
+public class Hello {
+
+	public static void main(final String[] args) {
+		//System.out.println("Not the right string, tests will fail!");
+		System.out.println("Hello world!");
+	}
+
+}
