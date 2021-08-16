4dbf53a051dc32770dc9107644d6750833cde534
Kevin Clevenger <kclevenger84@gmail.com>
2021-08-16 12:12:59 -0500

    Update Hello.java
    testing github desktop and VSC

--- src/main/java/com/bssd/Hello.java
+++ src/main/java/com/bssd/Hello.java
@@ -3,8 +3,8 @@ package com.bssd;
 public class Hello {
 
 	public static void main(final String[] args) {
-		//System.out.println("Not the right string, tests will fail!");
-		System.out.println("Hello world!");
+		System.out.println("Not the right string, tests will fail!");
+		//System.out.println("Hello world!");
 		//Just adding a comment in order to make a commit
 	}
 
