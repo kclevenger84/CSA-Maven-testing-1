aa145a7d564669ba795b207d92b71e5354573e81
Kevin Clevenger <kclevenger84@gmail.com>
2021-08-16 12:10:31 -0500

    added comment to test

--- src/main/java/com/bssd/Hello.java
+++ src/main/java/com/bssd/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(final String[] args) {
 		//System.out.println("Not the right string, tests will fail!");
 		System.out.println("Hello world!");
+		//Just adding a comment in order to make a commit
 	}
 
 }
